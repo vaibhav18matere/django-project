1af7f0bfa1fa536dd212e46e580ce42e4c950ece
VaibhavMatere <vpm00718897@gmail.com>
2026-02-23 22:49:30 +0530

    templates

--- sample/sample/settings.py
+++ sample/sample/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sample.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sample/sample/views.py
+++ sample/sample/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-     return HttpResponse("Hello World!!")
+     # return HttpResponse("Hello World!!")
+     return render(request, 'website/index.html')
 
 def about(request):
      return HttpResponse("About us")
